faf4fc0a90acad9b43a17fedfe9652ca247c8f66
Tyler Neumann <neumanntylers@gmail.com>
2021-02-07 00:22:14 -0500

    Fully implements first trick in fsm

---
+++ Assets/Scripts/Rotations/Rotate_LeftArm_AroundShoulder_Backward.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_LeftArm_AroundShoulder_Backward : StateMachineBehaviour
+{
+    private BodyParts bodyParts;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       bodyParts = animator.gameObject.GetComponent<BodyParts>();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       bodyParts.RotateBodyPartRelative
+       (
+           bodyParts.leftArm,
+           bodyParts.leftShoulder,
+           SpinDirections.Backward,
+           animator.GetInteger("Left_ArmSpinModifier")
+       );
+    }
+}
---
+++ Assets/Scripts/Rotations/Rotate_LeftProp_AroundHand_Forward.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_LeftProp_AroundHand_Forward : StateMachineBehaviour
+{
+    private BodyParts bodyParts;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       bodyParts = animator.gameObject.GetComponent<BodyParts>();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       bodyParts.RotateBodyPartRelative
+       (
+           bodyParts.leftProp,
+           bodyParts.leftHand,
+           SpinDirections.Forward,
+           animator.GetInteger("Left_PropSpinModifier")
+       );
+
+       animator.SetInteger("EighthStep", EnvironmentVariables.instance.eigthSteps);
+    }
+}
