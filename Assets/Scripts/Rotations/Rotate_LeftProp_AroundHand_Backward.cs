4b3425b705f593cbfee16f1d5405659852019130
Tyler Neumann <neumanntylers@gmail.com>
2021-02-07 19:44:02 -0500

    First implementation of new pattern. Adds wheel-inspin-flower.

---
+++ Assets/Scripts/Rotations/Rotate_LeftProp_AroundHand_Backward.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_LeftProp_AroundHand_Backward : StateMachineBehaviour
+{
+    private BodyParts bodyParts;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       bodyParts = animator.gameObject.GetComponent<BodyParts>();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       bodyParts.RotateBodyPartRelative
+       (
+           bodyParts.leftProp,
+           bodyParts.leftHand,
+           SpinDirections.Backward,
+           animator.GetInteger("Left_PropSpinModifier")
+       );
+    }
+}
---
+++ Assets/Scripts/Rotations/Rotate_RightProp_AroundHand_Forward.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_RightProp_AroundHand_Forward : StateMachineBehaviour
+{
+    private BodyParts bodyParts;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       bodyParts = animator.gameObject.GetComponent<BodyParts>();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       bodyParts.RotateBodyPartRelative
+       (
+           bodyParts.rightProp,
+           bodyParts.rightHand,
+           SpinDirections.Forward,
+           animator.GetInteger("Right_PropSpinModifier")
+       );
+    }
+}
